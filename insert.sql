f4f50099c73048b65844386e62dd09a2624c9382
honghayeon <41175122+honghayeon@users.noreply.github.com>
2019-12-05 13:39:44 +0900

    backup

---
+++ insert.sql
@@ -0,0 +1,72 @@
+use webtoon;
+
+insert into reader values
+(1, 20, "남"),
+(2, 34, "여"),
+(3, 23, "남"),
+(4, 17, "남"),
+(5, 8, "여");
+
+insert into writer values
+(1, "SIU", "이종휘", "inutero3334@naver.com"),
+(2, "조석", "조석", "jsinvade@naver.com"),
+(3, "야옹이", "김나영", "wirter01@naver.com"),
+(4, "기안84", "김희민", "khmnim84@naver.com"),
+(5, "박태준", "박태준", "thesooha@naver.com"),
+(6, "한경찰", "한경찰", "hedaa2002@naver.com"),
+(7, "yujoo5208", null, null),
+(8, "hanmoivs", null, null),
+(9, "chue0513", null, null),
+(10, "kaapuchino", null, null),
+(11, "zack5594", null, null);
+
+insert into webtoon values
+(1, "신의 탑", 1, "2010-06-30", 0),
+(2, "마음의 소리", 2, "2006-09-08", 0),
+(3, "여신강림", 3, "2018-04-02", 0),
+(4, "복학왕", 5, "2014-06-10", 0),
+(5, "외모지상주의", 4, "2014-11-20", 0),
+(6, "스피릿 핑거스", 6, "2016-03-16", 1);
+
+insert into buy values
+(1, 3, 2, 3000),
+(2, 1, 4, 10000),
+(3, 5, 5, 50000),
+(4, 4, 3, 2000),
+(5, 3, 1, 5000),
+(6, 2, 3, 8900);
+
+insert into challenge values
+(1, "알콜일기", 6),
+(2, "살문", 7),
+(3, "크리비 (Crea.B)", 8),
+(4, "도깨비불", 9),
+(5, "킬러5", 10);
+
+insert into comment values
+(1, 2, null, 3, 0, 0, 0),
+(2, 4, null, 5, 232, 11, 1),
+(3, 1, null, 2, 1, 0, 0),
+(4, null, 3, 3, 55, 2, 0),
+(5, null, 5, 4, 2, 1, 0);
+
+insert into login values
+(1, "test1", "test1"),
+(2, "test2", "test2"),
+(3, "test3", "test3"),
+(4, "test4", "test4"),
+(5, "test5", "test5"),
+(6, "test6", "test6"),
+(7, "test7", "test7"),
+(8, "test8", "test8"),
+(9, "test9", "test9"),
+(10, "test10", "test10"),
+(11, "test11", "test11");
+
+insert into manager values
+(1, "김땡땡", 1),
+(2, "이땡땡", 2),
+(3, "신땡땡", 3),
+(4, "홍땡땡", 4),
+(5, "최땡땡", 5);
+
---
+++ webtoon.sql
@@ -0,0 +1,173 @@
+-- MySQL Workbench Forward Engineering
+
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
+
+-- -----------------------------------------------------
+-- Schema mydb
+-- -----------------------------------------------------
+-- -----------------------------------------------------
+-- Schema webtoon
+-- -----------------------------------------------------
+
+-- -----------------------------------------------------
+-- Schema webtoon
+-- -----------------------------------------------------
+CREATE SCHEMA IF NOT EXISTS `webtoon` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci ;
+USE `webtoon` ;
+
+-- -----------------------------------------------------
+-- Table `webtoon`.`reader`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `webtoon`.`reader` (
+  `readerid` INT(11) NOT NULL AUTO_INCREMENT,
+  `age` INT(11) NULL DEFAULT NULL,
+  `gender` VARCHAR(45) NULL DEFAULT NULL,
+  PRIMARY KEY (`readerid`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
+
+
+-- -----------------------------------------------------
+-- Table `webtoon`.`writer`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `webtoon`.`writer` (
+  `writerid` INT(11) NOT NULL AUTO_INCREMENT,
+  `nick` VARCHAR(45) NOT NULL,
+  `writername` VARCHAR(45) NULL DEFAULT NULL,
+  `email` VARCHAR(45) NULL DEFAULT NULL,
+  PRIMARY KEY (`writerid`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
+
+
+-- -----------------------------------------------------
+-- Table `webtoon`.`webtoon`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `webtoon`.`webtoon` (
+  `wtoonid` INT(11) NOT NULL,
+  `wtoonname` VARCHAR(45) NOT NULL,
+  `we_writerid` INT(11) NOT NULL,
+  `1st_date` DATE NOT NULL,
+  `compl` TINYINT(4) NULL DEFAULT 0,
+  PRIMARY KEY (`wtoonid`),
+  INDEX `fk_webtoon_writer1_idx` (`we_writerid` ASC) VISIBLE,
+  CONSTRAINT `fk_webtoon_writer1`
+    FOREIGN KEY (`we_writerid`)
+    REFERENCES `webtoon`.`writer` (`writerid`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
+
+
+-- -----------------------------------------------------
+-- Table `webtoon`.`buy`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `webtoon`.`buy` (
+  `buyid` INT(11) NOT NULL AUTO_INCREMENT,
+  `b_readerid` INT(11) NOT NULL,
+  `b_wtoonid` INT(11) NOT NULL,
+  `price` INT(11) NOT NULL,
+  PRIMARY KEY (`buyid`),
+  INDEX `fk_webtoon_has_reader_reader1_idx` (`b_readerid` ASC) VISIBLE,
+  INDEX `fk_webtoon_has_reader_webtoon1_idx` (`b_wtoonid` ASC) VISIBLE,
+  CONSTRAINT `fk_webtoon_has_reader_reader1`
+    FOREIGN KEY (`b_readerid`)
+    REFERENCES `webtoon`.`reader` (`readerid`),
+  CONSTRAINT `fk_webtoon_has_reader_webtoon1`
+    FOREIGN KEY (`b_wtoonid`)
+    REFERENCES `webtoon`.`webtoon` (`wtoonid`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
+
+
+-- -----------------------------------------------------
+-- Table `webtoon`.`challenge`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `webtoon`.`challenge` (
+  `chalid` INT(11) NOT NULL AUTO_INCREMENT,
+  `chalname` VARCHAR(45) NOT NULL,
+  `ch_writerid` INT(11) NOT NULL,
+  PRIMARY KEY (`chalid`),
+  INDEX `fk_challenge_writer1_idx` (`ch_writerid` ASC) VISIBLE,
+  CONSTRAINT `fk_challenge_writer1`
+    FOREIGN KEY (`ch_writerid`)
+    REFERENCES `webtoon`.`writer` (`writerid`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
+
+
+-- -----------------------------------------------------
+-- Table `webtoon`.`comment`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `webtoon`.`comment` (
+  `cmntid` INT(11) NOT NULL AUTO_INCREMENT,
+  `co_wtoonid` INT(11) NULL DEFAULT NULL,
+  `co_chalid` INT(11) NULL DEFAULT NULL,
+  `co_readerid` INT(11) NOT NULL,
+  `good` INT(11) NULL DEFAULT '0',
+  `bad` INT(11) NULL DEFAULT '0',
+  `best` TINYINT(4) NULL DEFAULT '0',
+  PRIMARY KEY (`cmntid`),
+  INDEX `fk_comment_webtoon1_idx` (`co_wtoonid` ASC) VISIBLE,
+  INDEX `fk_comment_challenge1_idx` (`co_chalid` ASC) VISIBLE,
+  INDEX `fk_comment_reader1_idx` (`co_readerid` ASC) VISIBLE,
+  CONSTRAINT `fk_comment_challenge1`
+    FOREIGN KEY (`co_chalid`)
+    REFERENCES `webtoon`.`challenge` (`chalid`),
+  CONSTRAINT `fk_comment_reader1`
+    FOREIGN KEY (`co_readerid`)
+    REFERENCES `webtoon`.`reader` (`readerid`),
+  CONSTRAINT `fk_comment_webtoon1`
+    FOREIGN KEY (`co_wtoonid`)
+    REFERENCES `webtoon`.`webtoon` (`wtoonid`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
+
+
+-- -----------------------------------------------------
+-- Table `webtoon`.`login`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `webtoon`.`login` (
+  `l_writerid` INT(11) NOT NULL,
+  `id` VARCHAR(45) NOT NULL,
+  `pwd` VARCHAR(45) NOT NULL,
+  INDEX `fk_login_writer1_idx` (`l_writerid` ASC) VISIBLE,
+  CONSTRAINT `fk_login_writer1`
+    FOREIGN KEY (`l_writerid`)
+    REFERENCES `webtoon`.`writer` (`writerid`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
+
+
+-- -----------------------------------------------------
+-- Table `webtoon`.`manager`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `webtoon`.`manager` (
+  `mngrid` INT(11) NOT NULL AUTO_INCREMENT,
+  `mngrname` VARCHAR(45) NOT NULL,
+  `m_wtoonid` INT(11) NOT NULL,
+  PRIMARY KEY (`mngrid`),
+  INDEX `fk_manager_webtoon1_idx` (`m_wtoonid` ASC) VISIBLE,
+  CONSTRAINT `fk_manager_webtoon1`
+    FOREIGN KEY (`m_wtoonid`)
+    REFERENCES `webtoon`.`webtoon` (`wtoonid`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
+
+
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
